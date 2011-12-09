52337401ddbac9790e61d3b9fa78396657c60530
k-ohtani <kazunori_ohtani@ecnavi.co.jp>
2011-12-09 16:39:56 +0900

    refactoring

--- src/main/java/bals/Balsable.java
+++
@@ -1,7 +0,0 @@
-package bals;
-
-public class Balsable {
-	public void balse() {
-		System.exit(1);
-	}
-}
--- src/test/java/balse/BalsableTest.java
+++ src/test/java/balse/BalsableTest.java
@@ -3,24 +3,13 @@ package balse;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import balse.Balsable;
-
 public class BalsableTest {
-
-	@Ignore
-	@Test
-	public void test() {
-		new Balsable().balse();
-		fail();
-	}
-	
 	@Test
 	public void testBalsableProcess() throws Exception {
-		ProcessBuilder pb = new ProcessBuilder("java",
-				"-classpath", "./target/test-classes;./target/classes",
+		ProcessBuilder pb = new ProcessBuilder("java", "-classpath",
+				"./target/test-classes;./target/classes",
 				"balse.BalsableRunner");
 		Process p = pb.start();
 		assertThat(p.waitFor(), is(1));
