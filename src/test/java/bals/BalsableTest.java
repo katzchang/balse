0cda699a274dd175e4526855bbc21676d9ff9797
k-ohtani <kazunori_ohtani@ecnavi.co.jp>
2011-12-09 16:21:26 +0900

    add characterizeProcess

--- src/test/java/bals/BalsableTest.java
+++ src/test/java/bals/BalsableTest.java
@@ -1,15 +1,25 @@
 package bals;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BalsableTest {
 
+	@Ignore
 	@Test
 	public void test() {
 		new Balsable().balse();
 		fail();
+	}
+	
+	@Test
+	public void characterizeProcess() throws Exception {
+		ProcessBuilder pb = new ProcessBuilder("java", "-version");
+		Process p = pb.start();
+		assertThat(p.waitFor(), is(0));
 	}
 
 }
